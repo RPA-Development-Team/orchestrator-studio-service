0f243581dce4cc34f9e1f6d955ef0c8de7b865b7
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-03-14 16:19:28 +0200

    save xamlFile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import required modules
 const express = require('express'); 
 const multer = require('multer'); // Middleware for handling multipart/form-data (i.e. file uploads)
+const fs = require('fs');
 const bodyParser = require('body-parser'); // Middleware for parsing HTTP request body
 const app = express(); 
 const path = require("path"); // Node.js module for working with file paths
@@ -15,7 +16,6 @@ let storage = multer.diskStorage({
     callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)) // Set the filename to "<fieldname>-<timestamp>.<extension>"
   }
 })
-
 // Create a multer instance that uses the configured storage options
 const upload = multer({ storage: storage });
 
@@ -23,16 +23,28 @@ const upload = multer({ storage: storage });
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define a route for handling form submissions via POST method
-app.post('/submit-form', upload.fields([{ name: 'profile_picture' }, { name: 'package' }]), (req, res) => {
+app.post('/submit-form', upload.fields([{ name: 'packageName' }, { name: 'date' }, { name: 'repeat' }]), (req, res) => {
   console.log("all data");
   console.log(req.body); // Log the parsed request body for debugging purposes
-  // console.log("uploaded file");
-  // console.log(req.file);
-  // console.log("Multiple files");
-  // console.log(req.files);
-  // console.log("Name"+req.body.name);
-
-  res.send('Form submitted successfully!') // Send a response indicating that the form was submitted successfully
+  
+  // Get the xamlFile value from the POST request
+  const xamlFile = req.body.xamlFile;
+  
+  // Create a file path for the new xaml file based on the current date and time
+  const filePath = `./uploads/xaml-${Date.now()}.xaml`;
+  
+  // Write the xamlFile value to the new file using Node's built-in file system module
+  fs.writeFile(filePath, xamlFile, function (err) {
+    if (err) {
+      // Handle any errors that occur during file write
+      console.error(err);
+      res.send('Error saving file');
+    } else {
+      // Send a response indicating that the file was saved successfully
+      console.log(`XAML file saved to ${filePath}`);
+      res.send('XAML file saved successfully');
+    }
+  });
 });
 
 // Start the server and listen on port 8000
