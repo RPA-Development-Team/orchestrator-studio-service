32589b4011825d898318921c68427e99fcf7c256
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-03-17 02:56:15 +0200

    connect to DB

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const { Storage } = require("@google-cloud/storage");
 const bodyParser = require("body-parser"); // Middleware for parsing HTTP request body
 const app = express();
 const path = require("path"); // Node.js module for working with file paths
+const dotenv = require('dotenv');
+dotenv.config();
+const dbConfig = require('./db/dbConfig');
+const { Pool } = require('pg');
+const pool = new Pool(dbConfig);
+
+
+
+
 
 // Configure multer to store uploaded files in the "uploads" directory, and generate a unique filename for each file
 let storage = multer.diskStorage({
@@ -27,19 +36,19 @@ const gc = new Storage({
   projectId: "encoded-mark-380613",
 });
 
+
+// to get the id = the name of bucket in gc 
+// gc.getBuckets().then(x => console.log(x)); 
+// id: 'orchestrator_bucket',
+
 const coolFilesBucket = gc.bucket("orchestrator_bucket");
 
 // Use the body-parser middleware to parse HTTP request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define a route for handling form submissions via POST method
-app.post(
-  "/submit-form",
-  upload.fields([
-    { name: "packageName" },
-    { name: "date" },
-    { name: "repeat" },
-  ]),
+app.post("/submit-form",
+  upload.fields([{ name: "packageName" }, {name:"xamlFile"},{ name: "date" }, { name: "repeat" }]),
   (req, res) => {
     console.log("all data");
     console.log(req.body); // Log the parsed request body for debugging purposes
@@ -48,7 +57,9 @@ app.post(
     const xamlFile = req.body.xamlFile;
 
     // Write the xamlFile value to the new file using Node's built-in file system module
-    const filePath = `./uploads/xaml-${Date.now()}.xaml`;
+    const xamlpath = `xaml-${Date.now()}.xaml`
+    const filePath = `./uploads/${xamlpath}`;
+
     fs.writeFile(filePath, xamlFile, function (err) {
       if (err) {
         // Handle any errors that occur during file write
@@ -56,16 +67,36 @@ app.post(
         res.send("Error saving file");
       } else {
         // Upload the xamlFile to Google Cloud Storage
-        coolFilesBucket.upload(filePath, {
-          gzip: true, 
-          metadata: {
-            cacheControl: "public, max-age=31536000", //This means that the file can be cached by any public client, such as a web browser, for up to 1 year
+        coolFilesBucket.upload(
+          filePath,
+          {
+            gzip: true,
+            metadata: {
+              cacheControl:
+                "public, max-age=31536000", //This means that the file can be cached by any public client, such as a web browser, for up to 1 year
+            },
           },
-        });
+        );
+          const pathDb = `http://orchestrator_bucket.storage.googleapis.com/${xamlpath} `
+        // Save the form data to the database
+        const { packageName, date, repeat } = req.body;
+        
 
-        // Send a response indicating that the file was saved successfully
-        console.log(`XAML file saved to ${filePath}`);
-        res.send("XAML file saved successfully");
+        pool.query(
+          "INSERT INTO processes(packageName, date, xamlFile, repeat) VALUES($1, $2, $3, $4)",
+          [packageName, date, pathDb, repeat],
+          (err, result) => {
+            if (err) {
+              console.log(err);
+              res.status(500).json({ error: "Internal server error" });
+            } else {
+              console.log("Form data saved successfully");
+              res
+                .status(200)
+                .json({ message: "Form data saved successfully" });
+            }
+          }
+        );
       }
     });
   }
