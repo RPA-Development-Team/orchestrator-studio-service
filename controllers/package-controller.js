d4d847d2e031733ac08fbddbfc5b20104966c460
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-06-26 03:07:19 +0300

    added update function

--- controllers/package-controller.js
+++ controllers/package-controller.js
@@ -96,55 +96,82 @@ exports.getAllPackages = async (req, res) => {
     }    
 }
 
-exports.createPackage = async (req, res) => {
+  exports.createPackage = async (req, res) => {
 
-    console.log("all data");
-    console.log(__dirname);
-    console.log(req.body); // Log the parsed request body for debugging purposes
-  
-    // Get the xamlFile value from the POST request
-    let xamlFile = req.body.xamlFile;
-    const buffer = Buffer.from(xamlFile,"base64");
-    xamlFile = buffer.toString("utf-8")
-    // console.log("XXXXXXX")
-    // console.log(xamlFile)
-  
-    // Write the xamlFile value to the new file using Node's built-in file system module
-    const xamlpath = `xaml-${Date.now()}.xaml`;
-    const filePath = path.join(__dirname, `../uploads/${xamlpath}`);
-  
-    fs.writeFile(filePath, xamlFile, async function (err) {
-      if (err) {
-        // Handle any errors that occur during file write
-        console.error(err);
-        res.send("Error saving file");
-      } else {
-        // Upload the xamlFile to Google Cloud Storage
-        coolFilesBucket.upload(filePath, {
-          gzip: true,
-          metadata: {
-            cacheControl:
-              "public, max-age=31536000", //This means that the file can be cached by any public client, such as a web browser, for up to 1 year
-          },
-        });
-        const pathDb = `http://orchestrator_bucket.storage.googleapis.com/${xamlpath} `;
-        // Save the form data to the database
-        const { packageName, date, time, userID } = req.body;
-        try {
-            await prisma.package.create({
-                data: {
-                    name: packageName,
-                    packageUrl: pathDb,
-                    userID: userID ? userID : 2
-                }
-            });
-            const machine_name = "Abdo-Machine";
-            const package = {package_name:packageName,machine_name,pathDb,date,time}
-            axios.post("http://orch-robot-service:8000/pkg", package)
-            res.status(200).json({ message: "Form data saved successfully" });
-        } catch (pErr) {
-            console.log(pErr)
+      console.log("all data");
+      console.log(req.body); // Log the parsed request body for debugging purposes
+    
+      // Get the xamlFile value from the POST request
+      let xamlFile = req.body.xamlFile;
+      const buffer = Buffer.from(xamlFile,"base64");
+      xamlFile = buffer.toString("utf-8")
+
+      // Write the xamlFile value to the new file using Node's built-in file system module
+      const xamlpath = `xaml-${Date.now()}.xaml`;
+      const filePath = path.join(__dirname, `../uploads/${xamlpath}`);
+    
+      fs.writeFile(filePath, xamlFile, async function (err) {
+        if (err) {
+          // Handle any errors that occur during file write
+          console.error(err);
+          res.send("Error saving file");
+        } else {
+          // Upload the xamlFile to Google Cloud Storage
+          // coolFilesBucket.upload(filePath, {
+          //   gzip: true,
+          //   metadata: {
+          //     cacheControl:
+          //       "public, max-age=31536000", //This means that the file can be cached by any public client, such as a web browser, for up to 1 year
+          //   },
+          // });
+          const pathDb = `http://orchestrator_bucket.storage.googleapis.com/${xamlpath} `;
+          // Save the form data to the database
+          const { packageName, date, time, userID, description } = req.body;
+          try {
+              await prisma.package.create({
+                  data: {
+                      name: packageName,
+                      packageUrl: pathDb,
+                      userID: userID ? userID : 2,
+                      description: description
+                  }
+              });
+              // const machine_name = "Abdo-Machine";
+              // const package = {package_name:packageName,machine_name,pathDb,date,time}
+              // axios.post("http://orch-robot-service:8000/pkg", package)
+              res.status(200).json({ message: "Form data saved successfully" });
+          } catch (pErr) {
+              console.log(pErr)
+          }
         }
-      }
-    });
-}
+      });
+  }
+
+  exports.updatePackage = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, description } = req.body;
+  
+      const updatedPackage = await prisma.package.update({
+        where: {
+          id: parseInt(id, 10)
+        },
+        data: {
+          name:name,
+          description: description
+        }
+      });
+  
+      return res.status(200).json({
+        message: "Package updated successfully",
+        updatedPackage: updatedPackage
+      });
+    } catch (err) {
+      console.log(`Error while updating package\nError: ${err.message}`);
+      return res.status(500).json({
+        message: "Failed to update package",
+        error: err.message
+      });
+    }
+  };
+  
