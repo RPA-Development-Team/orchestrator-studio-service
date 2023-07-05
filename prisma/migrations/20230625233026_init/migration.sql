5b832036663d3877d5d8d4450ed733cbda07fcbd
khalidzahra <khalidzahra923@gmail.com>
2023-07-05 05:05:26 +0300

    Update prisma schema

--- prisma/migrations/20230625233026_init/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE useraccount_id_seq;
-ALTER TABLE "UserAccount" ALTER COLUMN "id" SET DEFAULT nextval('useraccount_id_seq');
-ALTER SEQUENCE useraccount_id_seq OWNED BY "UserAccount"."id";
